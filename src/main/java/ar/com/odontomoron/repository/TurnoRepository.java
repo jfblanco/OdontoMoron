ebd9647ca91f591a8cb3984c4bd2688554d2aecc
jfblanco <blanco.jose.francisco@gmail.com>
2015-09-27 18:53:52 -0300

    Se agregaron la lista de Turnos por odontologo

--- src/main/java/ar/com/odontomoron/repository/TurnoRepository.java
+++ src/main/java/ar/com/odontomoron/repository/TurnoRepository.java
@@ -4,10 +4,12 @@ import ar.com.odontomoron.domain.Turno;
 import org.springframework.data.jpa.repository.*;
 
 import java.util.List;
+import org.joda.time.DateTime;
 
 /**
  * Spring Data JPA repository for the Turno entity.
  */
 public interface TurnoRepository extends JpaRepository<Turno,Long> {
 
+    public List<Turno> findByFechaAndOdontologo(DateTime fecha, Long odontologoId);
 }
--- src/main/java/ar/com/odontomoron/web/rest/TurnoResource.java
+++ src/main/java/ar/com/odontomoron/web/rest/TurnoResource.java
@@ -2,8 +2,10 @@ package ar.com.odontomoron.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import ar.com.odontomoron.domain.Turno;
+import ar.com.odontomoron.domain.User;
 import ar.com.odontomoron.repository.PacienteRepository;
 import ar.com.odontomoron.repository.TurnoRepository;
+import ar.com.odontomoron.repository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
+import org.joda.time.DateTime;
 
 /**
  * REST controller for managing Turno.
@@ -31,6 +34,9 @@ public class TurnoResource {
     
     @Inject
     private PacienteRepository pacienteRepository;
+    
+    @Inject
+    private UserRepository userRepository;
 
     /**
      * POST  /turnos -> Create a new turno.
@@ -87,6 +93,23 @@ public class TurnoResource {
     public ResponseEntity<Turno> get(@PathVariable Long id) {
         log.debug("REST request to get Turno : {}", id);
         return Optional.ofNullable(turnoRepository.findOne(id))
+            .map(turno -> new ResponseEntity<>(
+                turno,
+                HttpStatus.OK))
+            .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+    
+    /**
+     * GET  /turnos/:numeroAsociado -> get turnos by odontologo's ID and today.
+     */
+    @RequestMapping(value = "/turnos/pacienteodontologo/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public ResponseEntity<List<Turno>> getPorOdontologo(@PathVariable Long id) {
+        User odontologo = userRepository.findOne(id);
+        log.debug("REST getting Pacientes for the Odontologo : {}", odontologo);
+        return Optional.ofNullable(turnoRepository.findByFechaAndOdontologo(new DateTime(),odontologo.getId()))
             .map(turno -> new ResponseEntity<>(
                 turno,
                 HttpStatus.OK))
